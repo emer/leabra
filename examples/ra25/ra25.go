475bc72f8dbfe59fe0627b586d1678b5ac1d552c
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2019-11-30 14:21:42 -0800

    use .tsv or .csv instead of .dat for all etable files -- makes it clearer what they are and that other programs can import

--- examples/ra25/ra25.go
+++ examples/ra25/ra25.go
@@ -727,7 +727,7 @@ func (ss *Sim) OpenPats() {
 	dt := ss.Pats
 	dt.SetMetaData("name", "TrainPats")
 	dt.SetMetaData("desc", "Training patterns")
-	err := dt.OpenCSV("random_5x5_25.dat", etable.Tab)
+	err := dt.OpenCSV("random_5x5_25.tsv", etable.Tab)
 	if err != nil {
 		log.Println(err)
 	}
