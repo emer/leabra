emer/leabra

commit e7eb4103bcb5c569ffbb68a55ae410203c9a525f

agate major updates

leabra/layer.go
@@ -1005,16 +1005,25 @@ func (ly *Layer) AvgMaxGe(ltime *Time) {
 	}
 }
 
-// InhibiFmGeAct computes inhibition Gi from Ge and Act averages within relevant Pools
+// InhibFmGeAct computes inhibition Gi from Ge and Act averages within relevant Pools
 func (ly *Layer) InhibFmGeAct(ltime *Time) {
 	lpl := &ly.Pools[0]
 	ly.Inhib.Layer.Inhib(&lpl.Inhib)
+	ly.PoolInhibFmGeAct(ltime)
+}
+
+// PoolInhibFmGeAct computes inhibition Gi from Ge and Act averages within relevant Pools
+func (ly *Layer) PoolInhibFmGeAct(ltime *Time) {
+	lpl := &ly.Pools[0]
 	np := len(ly.Pools)
 	if np > 1 {
 		for pi := 1; pi < np; pi++ {
 			pl := &ly.Pools[pi]
 			ly.Inhib.Pool.Inhib(&pl.Inhib)
 			pl.Inhib.Gi = math32.Max(pl.Inhib.Gi, lpl.Inhib.Gi)
+			if !ly.Inhib.Layer.On { // keep layer level updated for inter-layer inhib
+				lpl.Inhib.Gi = math32.Max(pl.Inhib.Gi, lpl.Inhib.Gi)
+			}
 			for ni := pl.StIdx; ni < pl.EdIdx; ni++ {
 				nrn := &ly.Neurons[ni]
 				if nrn.IsOff() {

agate/network.go
@@ -74,18 +74,33 @@ func (nt *Network) AddPFC(name string, nPoolsY, nPoolsX, nNeurY, nNeurX int, pul
 //         for mixing in to other models
 
 // AddMaintLayer adds a MaintLayer using 4D shape with pools,
-// and lateral PoolOneToOne connectivity.
+// and lateral NMDAMaint PoolOneToOne connectivity.
 func AddMaintLayer(nt *leabra.Network, name string, nPoolsY, nPoolsX, nNeurY, nNeurX int) *MaintLayer {
 	ly := &MaintLayer{}
 	nt.AddLayerInit(ly, name, []int{nPoolsY, nPoolsX, nNeurY, nNeurX}, emer.Hidden)
-	nt.ConnectLayers(ly, ly, prjn.NewPoolOneToOne(), emer.Lateral)
+	ConnectNMDA(nt, ly, ly, prjn.NewPoolOneToOne())
 	return ly
+}
+
+// AddOutLayer adds a OutLayer using 4D shape with pools,
+// and lateral PoolOneToOne connectivity.
+func AddOutLayer(nt *leabra.Network, name string, nPoolsY, nPoolsX, nNeurY, nNeurX int) *OutLayer {
+	ly := &OutLayer{}
+	nt.AddLayerInit(ly, name, []int{nPoolsY, nPoolsX, nNeurY, nNeurX}, emer.Hidden)
+	return ly
+}
+
+// ConnectNMDA adds a NMDAMaintPrjn between given layers
+func ConnectNMDA(nt *leabra.Network, send, recv emer.Layer, pat prjn.Pattern) emer.Prjn {
+	return nt.ConnectLayersPrjn(send, recv, pat, NMDAMaint, &NMDAMaintPrjn{})
 }
 
 // AddPFC adds a PFC system including SuperLayer, CT with CTCtxtPrjn, MaintLayer,
 // and OutLayer which is gated by BG.
 // Name is set to "PFC" if empty.  Other layers have appropriate suffixes.
 // Optionally creates a TRC Pulvinar for Super.
+// Standard Deep CTCtxtPrjn PoolOneToOne Super -> CT projection, and
+// 1to1 projections Super -> Maint and Maint -> Out class PFCFixed are created by default.
 // CT is placed Behind Super, then Out and Maint, and Pulvinar behind CT if created.
 func AddPFC(nt *leabra.Network, name string, nPoolsY, nPoolsX, nNeurY, nNeurX int, pulvLay bool) (super, ct, maint, out, pulv emer.Layer) {
 	if name == "" {
@@ -93,14 +108,24 @@ func AddPFC(nt *leabra.Network, name string, nPoolsY, nPoolsX, nNeurY, nNeurX in
 	}
 	super = deep.AddSuperLayer4D(nt, name, nPoolsY, nPoolsX, nNeurY, nNeurX)
 	ct = deep.AddCTLayer4D(nt, name+"CT", nPoolsY, nPoolsX, nNeurY, nNeurX)
-	maint = AddMaintLayer(nt, name+"Mnt", nPoolsY, nPoolsX, nNeurY, nNeurX)
-	out = nt.AddLayer4D(name+"Out", nPoolsY, nPoolsX, nNeurY, nNeurX, emer.Hidden)
+	mainti := AddMaintLayer(nt, name+"Mnt", nPoolsY, nPoolsX, nNeurY, nNeurX)
+	maint = mainti
+	outi := AddOutLayer(nt, name+"Out", nPoolsY, nPoolsX, nNeurY, nNeurX)
+	out = outi
 
 	ct.SetRelPos(relpos.Rel{Rel: relpos.Behind, Other: name, XAlign: relpos.Left, Space: 2})
 	out.SetRelPos(relpos.Rel{Rel: relpos.RightOf, Other: name, YAlign: relpos.Front, Space: 2})
 	maint.SetRelPos(relpos.Rel{Rel: relpos.Behind, Other: out.Name(), XAlign: relpos.Left, Space: 2})
 
+	one2one := prjn.NewOneToOne()
 	deep.ConnectCtxtToCT(nt, super, ct, prjn.NewPoolOneToOne())
+
+	pj := nt.ConnectLayers(super, maint, one2one, emer.Forward)
+	pj.SetClass("PFCFixed")
+	pj = nt.ConnectLayers(maint, out, one2one, emer.Forward)
+	pj.SetClass("PFCFixed")
+	outi.Out.MaintLay = maint.Name()
+	mainti.InterInhib.Lays.Add(out.Name())
 
 	if pulvLay {
 		pulvi := deep.AddTRCLayer4D(nt, name+"P", nPoolsY, nPoolsX, nNeurY, nNeurX)
@@ -110,66 +135,3 @@ func AddPFC(nt *leabra.Network, name string, nPoolsY, nPoolsX, nNeurY, nNeurX in
 	}
 	return
 }
-
-/*
-
-// AddPFCLayer adds a PFCLayer, super and deep, of given size, with given name.
-// nY, nX = number of pools in Y, X dimensions, and each pool has nNeurY, nNeurX neurons.
-// out is true for output-gating layer. Both have the class "PFC" set.
-// deep receives one-to-one projections of class "PFCToDeep" from super, and sends "PFCFmDeep",
-// and is positioned behind it.
-func (nt *Network) AddPFCLayer(name string, nY, nX, nNeurY, nNeurX int, out bool) (sp, dp *PFCLayer) {
-	sp = &PFCLayer{}
-	nt.AddLayerInit(sp, name, []int{nY, nX, nNeurY, nNeurX}, emer.Hidden)
-	dp = &PFCLayer{}
-	nt.AddLayerInit(dp, name+"D", []int{nY, nX, nNeurY, nNeurX}, deep.Deep)
-	sp.SetClass("PFC")
-	dp.SetClass("PFC")
-	sp.Gate.OutGate = out
-	dp.Gate.OutGate = out
-	dp.Dyns.MaintOnly()
-	dp.SetRelPos(relpos.Rel{Rel: relpos.Behind, Other: name, XAlign: relpos.Left, Space: 2})
-	pj := nt.ConnectLayers(sp, dp, prjn.NewOneToOne(), deep.BurstCtxt)
-	pj.SetClass("PFCToDeep")
-	pj = nt.ConnectLayers(dp, sp, prjn.NewOneToOne(), deep.DeepAttn)
-	pj.SetClass("PFCFmDeep")
-	return
-}
-
-// AddPFC adds paired PFCmnt, PFCout and associated Deep layers,
-// with given optional prefix.
-// nY = number of pools in Y dimension, nMaint, nOut are pools in X dimension,
-// and each pool has nNeurY, nNeurX neurons.  Appropriate PoolOneToOne connections
-// are made within super / deep (see AddPFCLayer) and between PFCmntD -> PFCout.
-func (nt *Network) AddPFC(prefix string, nY, nMaint, nOut, nNeurY, nNeurX int) (pfcMnt, pfcMntD, pfcOut, pfcOutD leabra.LeabraLayer) {
-	if prefix == "" {
-		prefix = "PFC"
-	}
-	if nMaint > 0 {
-		pfcMnt, pfcMntD = nt.AddPFCLayer(prefix+"mnt", nY, nMaint, nNeurY, nNeurX, false)
-	}
-	if nOut > 0 {
-		pfcOut, pfcOutD = nt.AddPFCLayer(prefix+"out", nY, nOut, nNeurY, nNeurX, true)
-	}
-	if pfcOut != nil && pfcMnt != nil {
-		pfcOut.SetRelPos(relpos.Rel{Rel: relpos.RightOf, Other: pfcMnt.Name(), YAlign: relpos.Front, Space: 2})
-		pj := nt.ConnectLayers(pfcMntD, pfcOut, prjn.NewOneToOne(), emer.Forward)
-		pj.SetClass("PFCMntDToOut")
-	}
-	return
-}
-
-// AddPBWM adds a DorsalBG an PFC with given params
-func (nt *Network) AddPBWM(prefix string, nY, nMaint, nOut, nNeurBgY, nNeurBgX, nNeurPfcY, nNeurPfcX int) (mtxGo, mtxNoGo, gpe, gpi, pfcMnt, pfcMntD, pfcOut, pfcOutD leabra.LeabraLayer) {
-	mtxGo, mtxNoGo, gpe, gpi = nt.AddDorsalBG(prefix, nY, nMaint, nOut, nNeurBgY, nNeurBgX)
-	pfcMnt, pfcMntD, pfcOut, pfcOutD = nt.AddPFC(prefix, nY, nMaint, nOut, nNeurPfcY, nNeurPfcX)
-	if pfcMnt != nil {
-		pfcMnt.SetRelPos(relpos.Rel{Rel: relpos.Above, Other: mtxGo.Name(), YAlign: relpos.Front, XAlign: relpos.Left})
-	}
-	gpl := gpi.(*GPiThalLayer)
-	gpl.SendToMatrixPFC(prefix) // sends gating to all these layers
-	gpl.SendGateShape()
-	return
-}
-
-*/

pcore/network.go
@@ -154,7 +154,10 @@ func AddBG(nt *leabra.Network, prefix string, nPoolsY, nPoolsX, nNeurY, nNeurX i
 	stns = AddSTNLayer(nt, prefix+"STNs", nPoolsY, nPoolsX, 1, 1)
 	mtxGo = AddMatrixLayer(nt, prefix+"MtxGo", nPoolsY, nPoolsX, nNeurY, nNeurX, D1R)
 	mtxNo = AddMatrixLayer(nt, prefix+"MtxNo", nPoolsY, nPoolsX, nNeurY, nNeurX, D2R)
-	cin = AddCINLayer(nt, prefix+"CIN")
+	cini := AddCINLayer(nt, prefix+"CIN")
+	cin = cini
+
+	cini.SendACh.Add(mtxGo.Name(), mtxNo.Name())
 
 	vthal.SetRelPos(relpos.Rel{Rel: relpos.RightOf, Other: gpi.Name(), YAlign: relpos.Front, Space: 2})
 

agate/maint.go
@@ -6,27 +6,29 @@ package agate
 
 import (
 	"github.com/chewxy/math32"
-	"github.com/emer/emergent/emer"
+	"github.com/emer/leabra/interinhib"
 	"github.com/emer/leabra/leabra"
 	"github.com/goki/ki/kit"
 )
 
-// NMDAParams control the NMDA dynamics in PFC Maint neurons, based on Brunel & Wang (2001)
+// MaintParams control the NMDA dynamics in PFC Maint neurons, based on Brunel & Wang (2001)
 // parameters.  We have to do some things to make it work for rate code neurons..
-type NMDAParams struct {
-	Tau   float32 `def:"100" desc:"decay time constant for NMDA current -- rise time is 2 msec and not worth extra effort for biexponential"`
-	Gbar  float32 `def:"1.7" desc:"strength of NMDA current -- 1.7 is just over level sufficient to maintain in face of completely blank input"`
-	ActVm float32 `def:"0.4" desc:"extra contribution to Vm associated with action potentials, on average -- produces key nonlinearity associated with spiking, from backpropagating action potentials.  0.4 seems good.."`
+type MaintParams struct {
+	Tau         float32 `def:"100" desc:"decay time constant for NMDA current -- rise time is 2 msec and not worth extra effort for biexponential"`
+	Gbar        float32 `def:"1.7" desc:"strength of NMDA current -- 1.7 is just over level sufficient to maintain in face of completely blank input"`
+	ActVm       float32 `def:"0.4" desc:"extra contribution to Vm associated with action potentials, on average -- produces key nonlinearity associated with spiking, from backpropagating action potentials.  0.4 seems good.."`
+	AlphaMaxCyc int     `desc:"cycle on which to start recording AlphaMax -- allow for transition"`
 }
 
-func (np *NMDAParams) Defaults() {
+func (np *MaintParams) Defaults() {
 	np.Tau = 100
 	np.Gbar = 1.7
 	np.ActVm = 0.4
+	np.AlphaMaxCyc = 30
 }
 
 // GFmV returns the NMDA conductance as a function of normalized membrane potential
-func (np *NMDAParams) GFmV(v float32) float32 {
+func (np *MaintParams) GFmV(v float32) float32 {
 	vbio := v*100 - 100
 	return 1 / (1 + 0.28*math32.Exp(-0.062*vbio))
 }
@@ -35,19 +37,24 @@ func (np *NMDAParams) GFmV(v float32) float32 {
 // MaintLayer
 
 // MaintLayer is a layer with NMDA channels that supports active maintenance
-// in frontal cortex, via layer 3 recurrent circuits.
+// in frontal cortex, via NMDA channels (in an NMDAMaintPrjn).
 type MaintLayer struct {
 	leabra.Layer
-	NMDA       NMDAParams    `view:"inline" desc:"parameters for NMDA channel conductances that sustain active maintenance"`
-	MaintNeurs []MaintNeuron `desc:"slice of extra MaintNeuron state for this layer -- flat list of len = Shape.Len(). You must iterate over index and use pointer to modify values."`
+	Maint      MaintParams           `view:"inline" desc:"maintenance parameters, including for NMDA channel conductances that sustain active maintenance"`
+	InterInhib interinhib.InterInhib `desc:"inhibition from output layer"`
+	MaintNeurs []MaintNeuron         `desc:"slice of extra MaintNeuron state for this layer -- flat list of len = Shape.Len(). You must iterate over index and use pointer to modify values."`
 }
 
 var KiT_MaintLayer = kit.Types.AddType(&MaintLayer{}, leabra.LayerProps)
 
 func (ly *MaintLayer) Defaults() {
 	ly.Layer.Defaults()
-	ly.NMDA.Defaults()
+	ly.Maint.Defaults()
+	ly.InterInhib.Defaults()
+	ly.InterInhib.Gi = 0.1
+	ly.InterInhib.Add = true
 	ly.Act.Init.Decay = 0
+	ly.Inhib.Pool.On = true
 }
 
 func (ly *MaintLayer) InitNMDA() {
@@ -57,6 +64,14 @@ func (ly *MaintLayer) InitNMDA() {
 		nrn.GrecInc = 0
 		nrn.Gnmda = 0
 		nrn.VmEff = 0
+	}
+}
+
+// InitAlphaMax initializes the AlphaMax to 0
+func (ly *MaintLayer) InitAlphaMax() {
+	for ni := range ly.MaintNeurs {
+		nrn := &ly.MaintNeurs[ni]
+		nrn.AlphaMax = 0
 	}
 }
 
@@ -71,6 +86,7 @@ func (ly *MaintLayer) InitGInc() {
 func (ly *MaintLayer) InitActs() {
 	ly.Layer.InitActs()
 	ly.InitNMDA()
+	ly.InitAlphaMax()
 }
 
 func (ly *MaintLayer) DecayState(decay float32) {
@@ -82,6 +98,14 @@ func (ly *MaintLayer) DecayState(decay float32) {
 		mnr.Gnmda -= decay * mnr.Gnmda
 		mnr.VmEff -= decay * mnr.VmEff
 	}
+}
+
+// AlphaCycInit handles all initialization at start of new input pattern, including computing
+// input scaling from running average activation etc.
+// should already have presented the external input to the network at this point.
+func (ly *MaintLayer) AlphaCycInit() {
+	ly.Layer.AlphaCycInit()
+	ly.InitAlphaMax()
 }
 
 // GFmInc integrates new synaptic conductances from increments sent during last SendGDelta.
@@ -99,7 +123,7 @@ func (ly *MaintLayer) RecvGInc(ltime *leabra.Time) {
 		if p.IsOff() {
 			continue
 		}
-		if p.Type() == emer.Lateral { // skip recurrent
+		if p.Type() == NMDAMaint { // skip NMDA
 			continue
 		}
 		p.(leabra.LeabraPrjn).RecvGInc()
@@ -112,7 +136,7 @@ func (ly *MaintLayer) RecvGrecInc(ltime *leabra.Time) {
 		if p.IsOff() {
 			continue
 		}
-		if p.Type() != emer.Lateral { // skip recurrent
+		if p.Type() != NMDAMaint { // skip non-NMDA
 			continue
 		}
 		pj := p.(leabra.LeabraPrjn).AsLeabra()
@@ -139,12 +163,56 @@ func (ly *MaintLayer) GFmIncNeur(ltime *leabra.Time) {
 		mnr := &ly.MaintNeurs[ni]
 		mnr.Grec += mnr.GrecInc
 		mnr.GrecInc = 0
-		mnr.VmEff = nrn.Vm + ly.NMDA.ActVm*nrn.Act
+		mnr.VmEff = nrn.Vm + ly.Maint.ActVm*nrn.Act
 
-		mnr.Gnmda = ly.NMDA.Gbar*mnr.Grec*ly.NMDA.GFmV(mnr.VmEff) - (mnr.Gnmda / ly.NMDA.Tau)
+		mnr.Gnmda = ly.Maint.Gbar*mnr.Grec*ly.Maint.GFmV(mnr.VmEff) - (mnr.Gnmda / ly.Maint.Tau)
 
 		ly.Act.GeFmRaw(nrn, nrn.GeRaw+mnr.Gnmda)
 	}
+}
+
+// InhibFmGeAct computes inhibition Gi from Ge and Act averages within relevant Pools
+func (ly *MaintLayer) InhibFmGeAct(ltime *leabra.Time) {
+	lpl := &ly.Pools[0]
+	ly.Inhib.Layer.Inhib(&lpl.Inhib)
+	ly.InterInhib.Inhib(&ly.Layer) // does inter-layer inhibition
+	ly.PoolInhibFmGeAct(ltime)
+}
+
+// ActFmG computes rate-code activation from Ge, Gi, Gl conductances
+// and updates learning running-average activations from that Act.
+// Maint extends to update AlphaMax
+func (ly *MaintLayer) ActFmG(ltime *leabra.Time) {
+	ly.Layer.ActFmG(ltime)
+	if ltime.Cycle > ly.Maint.AlphaMaxCyc {
+		ly.AlphaMaxFmAct(ltime)
+	}
+}
+
+// AlphaMaxFmAct computes AlphaMax from Activation
+func (ly *MaintLayer) AlphaMaxFmAct(ltime *leabra.Time) {
+	for ni := range ly.Neurons {
+		nrn := &ly.Neurons[ni]
+		if nrn.IsOff() {
+			continue
+		}
+		mnr := &ly.MaintNeurs[ni]
+		mnr.AlphaMax = math32.Max(mnr.AlphaMax, nrn.Act)
+	}
+}
+
+// MaxAlphaMax returns the maximum AlphaMax across the layer
+func (ly *MaintLayer) MaxAlphaMax() float32 {
+	mx := float32(0)
+	for ni := range ly.Neurons {
+		nrn := &ly.Neurons[ni]
+		if nrn.IsOff() {
+			continue
+		}
+		mnr := &ly.MaintNeurs[ni]
+		mx = math32.Max(mnr.AlphaMax, mx)
+	}
+	return mx
 }
 
 ///////////////////////////////////////////////////////////////////////////

agate/neuron.go
@@ -14,7 +14,7 @@ import (
 
 var (
 	// NeuronVars are extra neuron variables for agate, on top of pcore
-	NeuronVars = []string{"Grec", "GrecInc", "Gnmda", "VmEff"}
+	NeuronVars = []string{"Grec", "GrecInc", "Gnmda", "VmEff", "AlphaMax"}
 
 	// NeuronVarsAll is the agate collection of all neuron-level vars
 	NeuronVarsAll []string
@@ -38,18 +38,19 @@ func init() {
 }
 
 //////////////////////////////////////////////////////////////////////
-// STN neurons
+// Maint neurons
 
 // MaintNeuron holds the extra neuron (unit) level variables for STN computation.
 type MaintNeuron struct {
-	Grec    float32 `desc:"recurrent-only (self) conductance"`
-	GrecInc float32 `desc:"increment for recurrent-only (self) conductance"`
-	Gnmda   float32 `desc:"NMDA conductance, total -- added directly to Ge as it has the same reversal potential."`
-	VmEff   float32 `desc:"Effective membrane potential, including simulated backpropagating action potential contribution from activity level."`
+	Grec     float32 `desc:"recurrent-only (self) conductance"`
+	GrecInc  float32 `desc:"increment for recurrent-only (self) conductance"`
+	Gnmda    float32 `desc:"NMDA conductance, total -- added directly to Ge as it has the same reversal potential."`
+	VmEff    float32 `desc:"Effective membrane potential, including simulated backpropagating action potential contribution from activity level."`
+	AlphaMax float32 `desc:"Maximum activation over Alpha cycle period"`
 }
 
 var (
-	MaintNeuronVars    = []string{"Grec", "GrecInc", "Gnmda", "VmEff"}
+	MaintNeuronVars    = []string{"Grec", "GrecInc", "Gnmda", "VmEff", "AlphaMax"}
 	MaintNeuronVarsMap map[string]int
 )
 

agate/nmda.go
@@ -1,0 +1,77 @@
+// Copyright (c) 2019, The Emergent Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package agate
+
+import (
+	"github.com/emer/emergent/emer"
+	"github.com/emer/leabra/deep"
+	"github.com/emer/leabra/leabra"
+	"github.com/goki/ki/ki"
+	"github.com/goki/ki/kit"
+)
+
+// NMDAMaintPrjn is a projection with strong NMDA maintenance channels.
+// It essentially marks a projection for special treatment in a MaintLayer
+// which actually does the NMDA computations.  Excitatory conductance is aggregated
+// separately for this projection.
+type NMDAMaintPrjn struct {
+	leabra.Prjn // access as .Prjn
+}
+
+var KiT_NMDAMaintPrjn = kit.Types.AddType(&NMDAMaintPrjn{}, PrjnProps)
+
+func (pj *NMDAMaintPrjn) Defaults() {
+	pj.Prjn.Defaults()
+	pj.WtInit.Mean = 0.5
+	pj.WtInit.Var = 0
+	// todo: learning off by default?
+}
+
+func (pj *NMDAMaintPrjn) UpdateParams() {
+	pj.Prjn.UpdateParams()
+}
+
+func (pj *NMDAMaintPrjn) Type() emer.PrjnType {
+	return NMDAMaint
+}
+
+func (pj *NMDAMaintPrjn) PrjnTypeName() string {
+	if pj.Typ < emer.PrjnTypeN {
+		return pj.Typ.String()
+	}
+	ptyp := PrjnType(pj.Typ)
+	ts := ptyp.String()
+	sz := len(ts)
+	if sz > 0 {
+		return ts[:sz-1] // cut off trailing _
+	}
+	return ""
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+//  PrjnType
+
+// PrjnType has the AGate extensions to the emer.PrjnType types, for gui
+type PrjnType deep.PrjnType
+
+//go:generate stringer -type=PrjnType
+
+var KiT_PrjnType = kit.Enums.AddEnumExt(deep.KiT_PrjnType, PrjnTypeN, kit.NotBitFlag, nil)
+
+// The AGate prjn types
+const (
+	// NMDAMaint are projections that have strong NMDA channels supporting maintenance
+	NMDAMaint emer.PrjnType = emer.PrjnType(deep.PrjnTypeN) + iota
+)
+
+// gui versions
+const (
+	NMDAMaint_ PrjnType = PrjnType(deep.PrjnTypeN) + iota
+	PrjnTypeN
+)
+
+var PrjnProps = ki.Props{
+	"EnumType:Typ": KiT_PrjnType,
+}

agate/out.go
@@ -1,0 +1,69 @@
+// Copyright (c) 2020, The Emergent Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package agate
+
+import (
+	"log"
+
+	"github.com/emer/leabra/leabra"
+	"github.com/goki/ki/kit"
+)
+
+// OutParams determine the behavior of OutLayer
+type OutParams struct {
+	MaintLay string  `desc:"name of corresponding MaintLayer that is reset when this layer gets activated"`
+	ResetThr float32 `desc:"threshold on activation, above which the MaintLay will be reset"`
+}
+
+func (np *OutParams) Defaults() {
+	np.ResetThr = 0.5
+	if np.MaintLay == "" {
+		np.MaintLay = "PFCMnt"
+	}
+}
+
+// OutLayer is a frontal cortex output layer (L5 PM), which typically is interconnected
+// with Ventral Thalamus (VM / VA etc) for output gating, and also NMDAPrjn maintenance.
+type OutLayer struct {
+	MaintLayer
+	Out OutParams `desc:"Parameters for output layer function"`
+}
+
+var KiT_OutLayer = kit.Types.AddType(&OutLayer{}, leabra.LayerProps)
+
+func (ly *OutLayer) Defaults() {
+	ly.MaintLayer.Defaults()
+	ly.Out.Defaults()
+}
+
+// MaintLay returns the MaintLay by name
+func (ly *OutLayer) MaintLay() (*MaintLayer, error) {
+	tly, err := ly.Network.LayerByNameTry(ly.Out.MaintLay)
+	if err != nil {
+		log.Printf("OutLayer %s, MaintLay: %v\n", ly.Name(), err)
+		return nil, err
+	}
+	return tly.(*MaintLayer), nil
+}
+
+// CyclePost calls ResetMaint
+func (ly *OutLayer) CyclePost(ltime *leabra.Time) {
+	ly.MaintLayer.CyclePost(ltime)
+	ly.ResetMaint(ltime)
+}
+
+// ResetMaint resets the maintenance layer if activation is above threshold
+func (ly *OutLayer) ResetMaint(ltime *leabra.Time) {
+	// todo: not sure if should be sub-pool or whole layer?
+
+	pl := ly.Pools[0]
+	maxact := pl.Inhib.Act.Max
+	if maxact > ly.Out.ResetThr {
+		mlay, err := ly.MaintLay()
+		if err == nil {
+			mlay.InitNMDA() // note: will continue to reset..
+		}
+	}
+}

agate/prjntype_string.go
@@ -1,0 +1,39 @@
+// Code generated by "stringer -type=PrjnType"; DO NOT EDIT.
+
+package agate
+
+import (
+	"errors"
+	"strconv"
+)
+
+var _ = errors.New("dummy error")
+
+func _() {
+	// An "invalid array index" compiler error signifies that the constant values have changed.
+	// Re-run the stringer command to generate them again.
+	var x [1]struct{}
+	_ = x[NMDAMaint_-5]
+	_ = x[PrjnTypeN-6]
+}
+
+const _PrjnType_name = "NMDAMaint_PrjnTypeN"
+
+var _PrjnType_index = [...]uint8{0, 10, 19}
+
+func (i PrjnType) String() string {
+	i -= 5
+	if i < 0 || i >= PrjnType(len(_PrjnType_index)-1) {
+		return "PrjnType(" + strconv.FormatInt(int64(i+5), 10) + ")"
+	}
+	return _PrjnType_name[_PrjnType_index[i]:_PrjnType_index[i+1]]
+}
+
+func StringToPrjnType(s string) (PrjnType, error) {
+	for i := 0; i < len(_PrjnType_index)-1; i++ {
+		if s == _PrjnType_name[_PrjnType_index[i]:_PrjnType_index[i+1]] {
+			return PrjnType(i + 5), nil
+		}
+	}
+	return 0, errors.New("String: " + s + " is not a valid option for type: PrjnType")
+}

commit 423191f846f19ac9fa6eedca6cff706af752623b

fixed important bug that probably didn't affect anyone!  bug was introduced in e7eb410 from 7/26.  Only affects inhibition in layers with pool-only, no layer-level inhibition (should be rare).  now fixed -- update if you might have had such a thing.

attrn/attn.go
@@ -67,7 +67,7 @@ type AttnSetLayer interface {
 type AttnLayer struct {
 	leabra.Layer
 	Attn  AttnParams `desc:"how Attention modulates Ge, Act and Lrn"`
-	Attns []float32  `desc:"per-pool attention modulation factors (normalized 0..1 range"`
+	Attns []float32  `desc:"per-pool attention modulation factors (normalized 0..1 range) -- note: index starts at 1 for pools"`
 }
 
 var KiT_AttnLayer = kit.Types.AddType(&AttnLayer{}, leabra.LayerProps)
@@ -89,9 +89,10 @@ func (ly *AttnLayer) InitActs() {
 	ly.InitAttn()
 }
 
-// SetAttn sets the attention value for given pool index
+// SetAttn sets the attention value for given pool index.
+// The pool index is in range 0..n-1 pools, not including the 0 = layer level pool
 func (ly *AttnLayer) SetAttn(pidx int, attn float32) {
-	ly.Attns[pidx] = attn
+	ly.Attns[pidx+1] = attn
 }
 
 // RecvGIncPrjn increments the receiver's GeInc or GiInc from that of all the projections.

attrn/trn.go
@@ -47,8 +47,12 @@ func (ly *TRNLayer) GeFmEPools() {
 			nrn := &ly.Neurons[ni]
 			opl := ol.Pools[1+ni]
 			nrn.GeRaw += ep.Wt * opl.Inhib.Act.Avg
-			ly.Act.GeFmRaw(nrn, nrn.GeRaw)
 		}
+	}
+	for ni := range ly.Neurons {
+		nrn := &ly.Neurons[ni]
+		ly.Act.GeFmRaw(nrn, nrn.GeRaw)
+		ly.Act.GiFmRaw(nrn, nrn.GiRaw)
 	}
 }
 

deep/network.go
@@ -137,14 +137,14 @@ func AddSuperCT2D(nt *leabra.Network, name string, shapeY, shapeX int, pulvLay b
 }
 
 // AddSuperCT4D adds a superficial (SuperLayer) and corresponding CT (CT suffix) layer
-// with CTCtxtPrjn Full projection from Super to CT.
+// with CTCtxtPrjn PoolOneToOne projection from Super to CT.
 // Optionally creates a TRC Pulvinar for Super.
 // CT is placed Behind Super, and Pulvinar behind CT if created.
 func AddSuperCT4D(nt *leabra.Network, name string, nPoolsY, nPoolsX, nNeurY, nNeurX int, pulvLay bool) (super, ct, pulv emer.Layer) {
 	super = AddSuperLayer4D(nt, name, nPoolsY, nPoolsX, nNeurY, nNeurX)
 	ct = AddCTLayer4D(nt, name+"CT", nPoolsY, nPoolsX, nNeurY, nNeurX)
 	ct.SetRelPos(relpos.Rel{Rel: relpos.Behind, Other: name, XAlign: relpos.Left, Space: 2})
-	ConnectCtxtToCT(nt, super, ct, prjn.NewFull())
+	ConnectCtxtToCT(nt, super, ct, prjn.NewPoolOneToOne())
 	if pulvLay {
 		pulvi := AddTRCLayer4D(nt, name+"P", nPoolsY, nPoolsX, nNeurY, nNeurX)
 		pulv = pulvi

leabra/layer.go
@@ -1026,9 +1026,10 @@ func (ly *Layer) PoolInhibFmGeAct(ltime *Time) {
 		for pi := 1; pi < np; pi++ {
 			pl := &ly.Pools[pi]
 			ly.Inhib.Pool.Inhib(&pl.Inhib)
-			pl.Inhib.Gi = math32.Max(pl.Inhib.Gi, lpl.Inhib.Gi)
-			if !ly.Inhib.Layer.On { // keep layer level updated for inter-layer inhib
-				lpl.Inhib.Gi = math32.Max(pl.Inhib.Gi, lpl.Inhib.Gi)
+			if ly.Inhib.Layer.On {
+				pl.Inhib.Gi = math32.Max(pl.Inhib.Gi, lpl.Inhib.Gi) // pool is max of layer
+			} else {
+				lpl.Inhib.Gi = math32.Max(pl.Inhib.Gi, lpl.Inhib.Gi) // update layer from pool
 			}
 			for ni := pl.StIdx; ni < pl.EdIdx; ni++ {
 				nrn := &ly.Neurons[ni]