fbebdf54f10dcbbcfbb62c4bf17cbdd1ead53a6e
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2021-08-11 00:47:22 -0700

    v1.1.36 release -- version.go updated

--- leabra/version.go
+++ leabra/version.go
@@ -3,8 +3,8 @@
 package leabra
 
 const (
-	Version     = "v1.1.35"
-	GitCommit   = "53660ac" // the commit JUST BEFORE the release
-	VersionDate = "2021-08-11 06:43" // UTC
+	Version     = "v1.1.36"
+	GitCommit   = "cac50f3" // the commit JUST BEFORE the release
+	VersionDate = "2021-08-11 07:47" // UTC
 )
 
