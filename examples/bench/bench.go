8bb0f26920cee7a86df2173e7ff7ee5c9c598370
Randall C. O'Reilly <oreilly@ucdavis.edu>
2024-10-22 17:20:35 -0700

    fix bench, remove deep -- confirmed working on sg  task

--- examples/bench/bench.go
+++ examples/bench/bench.go
@@ -15,10 +15,10 @@ import (
 	"math"
 	"math/rand"
 	"os"
+	"time"
 
 	"cogentcore.org/core/base/randx"
 	"cogentcore.org/core/base/timer"
-	"cogentcore.org/core/tensor"
 	"cogentcore.org/core/tensor/table"
 	"github.com/emer/emergent/v2/params"
 	"github.com/emer/emergent/v2/patgen"
@@ -29,44 +29,33 @@ import (
 var Net *leabra.Network
 var Pats *table.Table
 var EpcLog *table.Table
-var Thread = false // much slower for small net
 var Silent = false // non-verbose mode -- just reports result
 
 var ParamSets = params.Sets{
-	{Name: "Base", Desc: "these are the best params", Sheets: params.Sheets{
-		"Network": &params.Sheet{
-			{Sel: "Path", Desc: "norm and momentum on works better, but wt bal is not better for smaller nets",
-				Params: params.Params{
-					"Path.Learn.Norm.On":     "true",
-					"Path.Learn.Momentum.On": "true",
-					"Path.Learn.WtBal.On":    "false",
-				}},
-			{Sel: "Layer", Desc: "using default 1.8 inhib for all of network -- can explore",
-				Params: params.Params{
-					"Layer.Inhib.Layer.Gi": "1.8",
-					"Layer.Act.Gbar.L":     "0.2", // original value -- makes HUGE diff on perf!
-				}},
-			{Sel: "#Output", Desc: "output definitely needs lower inhib -- true for smaller layers in general",
-				Params: params.Params{
-					"Layer.Inhib.Layer.Gi": "1.4",
-				}},
-			{Sel: ".Back", Desc: "top-down back-pathways MUST have lower relative weight scale, otherwise network hallucinates",
-				Params: params.Params{
-					"Path.WtScale.Rel": "0.2",
-				}},
-		},
-		"Sim": &params.Sheet{ // sim params apply to sim object
-			{Sel: "Sim", Desc: "best params always finish in this time",
-				Params: params.Params{
-					"Sim.MaxEpcs": "50",
-				}},
-		},
-	}},
+	"Base": {
+		{Sel: "Path", Desc: "norm and momentum on works better, but wt bal is not better for smaller nets",
+			Params: params.Params{
+				"Path.Learn.Norm.On":     "true",
+				"Path.Learn.Momentum.On": "true",
+				"Path.Learn.WtBal.On":    "false",
+			}},
+		{Sel: "Layer", Desc: "using default 1.8 inhib for all of network -- can explore",
+			Params: params.Params{
+				"Layer.Inhib.Layer.Gi": "1.8",
+				"Layer.Act.Gbar.L":     "0.2", // original value -- makes HUGE diff on perf!
+			}},
+		{Sel: "#Output", Desc: "output definitely needs lower inhib -- true for smaller layers in general",
+			Params: params.Params{
+				"Layer.Inhib.Layer.Gi": "1.4",
+			}},
+		{Sel: ".Back", Desc: "top-down back-pathways MUST have lower relative weight scale, otherwise network hallucinates",
+			Params: params.Params{
+				"Path.WtScale.Rel": "0.2",
+			}},
+	},
 }
 
-func ConfigNet(net *leabra.Network, threads, units int) {
-	net.InitName(net, "BenchNet")
-
+func ConfigNet(net *leabra.Network, units int) {
 	squn := int(math.Sqrt(float64(units)))
 	shp := []int{squn, squn}
 
@@ -81,22 +70,12 @@ func ConfigNet(net *leabra.Network, threads, units int) {
 	net.ConnectLayers(hid2Lay, hid3Lay, paths.NewFull(), leabra.ForwardPath)
 	net.ConnectLayers(hid3Lay, outLay, paths.NewFull(), leabra.ForwardPath)
 
-	net.ConnectLayers(outLay, hid3Lay, paths.NewFull(), BackPath)
-	net.ConnectLayers(hid3Lay, hid2Lay, paths.NewFull(), BackPath)
-	net.ConnectLayers(hid2Lay, hid1Lay, paths.NewFull(), BackPath)
-
-	switch threads {
-	case 2:
-		hid3Lay.(leabra.LeabraLayer).SetThread(1)
-		outLay.(leabra.LeabraLayer).SetThread(1)
-	case 4:
-		hid2Lay.(leabra.LeabraLayer).SetThread(1)
-		hid3Lay.(leabra.LeabraLayer).SetThread(2)
-		outLay.(leabra.LeabraLayer).SetThread(3)
-	}
+	net.ConnectLayers(outLay, hid3Lay, paths.NewFull(), leabra.BackPath)
+	net.ConnectLayers(hid3Lay, hid2Lay, paths.NewFull(), leabra.BackPath)
+	net.ConnectLayers(hid2Lay, hid1Lay, paths.NewFull(), leabra.BackPath)
 
 	net.Defaults()
-	net.ApplyParams(ParamSets[0].Sheets["Network"], false) // no msg
+	net.ApplyParams(ParamSets["Base"], false) // no msg
 	net.Build()
 	net.InitWeights()
 }
@@ -106,54 +85,51 @@ func ConfigPats(dt *table.Table, pats, units int) {
 	shp := []int{squn, squn}
 	// fmt.Printf("shape: %v\n", shp)
 
-	dt.SetFromSchema(table.Schema{
-		{"Name", tensor.STRING, nil, nil},
-		{"Input", tensor.FLOAT32, shp, []string{"Y", "X"}},
-		{"Output", tensor.FLOAT32, shp, []string{"Y", "X"}},
-	}, pats)
+	dt.AddStringColumn("Name")
+	dt.AddFloat32TensorColumn("Input", shp)
+	dt.AddFloat32TensorColumn("Output", shp)
+	dt.SetNumRows(pats)
 
 	// note: actually can learn if activity is .15 instead of .25
 	// but C++ benchmark is for .25..
 	nOn := units / 6
 
-	patgen.PermutedBinaryRows(dt.Cols[1], nOn, 1, 0)
-	patgen.PermutedBinaryRows(dt.Cols[2], nOn, 1, 0)
+	patgen.PermutedBinaryRows(dt.Columns[1], nOn, 1, 0)
+	patgen.PermutedBinaryRows(dt.Columns[2], nOn, 1, 0)
 }
 
 func ConfigEpcLog(dt *table.Table) {
-	dt.SetFromSchema(table.Schema{
-		{"Epoch", tensor.INT64, nil, nil},
-		{"CosDiff", tensor.FLOAT32, nil, nil},
-		{"AvgCosDiff", tensor.FLOAT32, nil, nil},
-		{"SSE", tensor.FLOAT32, nil, nil},
-		{"Avg SSE", tensor.FLOAT32, nil, nil},
-		{"Count Err", tensor.FLOAT32, nil, nil},
-		{"Pct Err", tensor.FLOAT32, nil, nil},
-		{"Pct Cor", tensor.FLOAT32, nil, nil},
-		{"Hid1 ActAvg", tensor.FLOAT32, nil, nil},
-		{"Hid2 ActAvg", tensor.FLOAT32, nil, nil},
-		{"Out ActAvg", tensor.FLOAT32, nil, nil},
-	}, 0)
+	dt.AddIntColumn("Epoch")
+	dt.AddFloat32Column("CosDiff")
+	dt.AddFloat32Column("AvgCosDiff")
+	dt.AddFloat32Column("SSE")
+	dt.AddFloat32Column("Avg SSE")
+	dt.AddFloat32Column("Count Err")
+	dt.AddFloat32Column("Pct Err")
+	dt.AddFloat32Column("Pct Cor")
+	dt.AddFloat32Column("Hid1 ActAvg")
+	dt.AddFloat32Column("Hid2 ActAvg")
+	dt.AddFloat32Column("Out ActAvg")
 }
 
 func TrainNet(net *leabra.Network, pats, epcLog *table.Table, epcs int) {
-	ctx := leabra.NewTime()
+	ctx := leabra.NewContext()
 	net.InitWeights()
 	np := pats.NumRows()
 	porder := rand.Perm(np) // randomly permuted order of ints
 
 	epcLog.SetNumRows(epcs)
 
-	inLay := net.LayerByName("Input").(*leabra.Layer)
-	hid1Lay := net.LayerByName("Hidden1").(*leabra.Layer)
-	hid2Lay := net.LayerByName("Hidden2").(*leabra.Layer)
-	outLay := net.LayerByName("Output").(*leabra.Layer)
+	inLay := net.LayerByName("Input")
+	hid1Lay := net.LayerByName("Hidden1")
+	hid2Lay := net.LayerByName("Hidden2")
+	outLay := net.LayerByName("Output")
 
 	_ = hid1Lay
 	_ = hid2Lay
 
-	inPats := pats.ColByName("Input").(*tensor.Float32)
-	outPats := pats.ColByName("Output").(*tensor.Float32)
+	inPats, _ := pats.ColumnByName("Input")
+	outPats, _ := pats.ColumnByName("Output")
 
 	tmr := timer.Time{}
 	tmr.Start()
@@ -182,7 +158,7 @@ func TrainNet(net *leabra.Network, pats, epcLog *table.Table, epcs int) {
 				ctx.QuarterInc()
 			}
 			net.DWt()
-			net.WtFmDWt()
+			net.WtFromDWt()
 			outCosDiff += outLay.CosDiff.Cos
 			pSSE, pAvgSSE := outLay.MSE(0.5)
 			sse += pSSE
@@ -197,29 +173,27 @@ func TrainNet(net *leabra.Network, pats, epcLog *table.Table, epcs int) {
 		pctErr := float64(cntErr) / float64(np)
 		pctCor := 1 - pctErr
 		// fmt.Printf("epc: %v  \tCosDiff: %v \tAvgCosDif: %v\n", epc, outCosDiff, outLay.CosDiff.Avg)
-		epcLog.SetCellFloat("Epoch", epc, float64(epc))
-		epcLog.SetCellFloat("CosDiff", epc, float64(outCosDiff))
-		epcLog.SetCellFloat("AvgCosDiff", epc, float64(outLay.CosDiff.Avg))
-		epcLog.SetCellFloat("SSE", epc, sse)
-		epcLog.SetCellFloat("Avg SSE", epc, avgSSE)
-		epcLog.SetCellFloat("Count Err", epc, float64(cntErr))
-		epcLog.SetCellFloat("Pct Err", epc, pctErr)
-		epcLog.SetCellFloat("Pct Cor", epc, pctCor)
-		epcLog.SetCellFloat("Hid1 ActAvg", epc, float64(hid1Lay.Pools[0].ActAvg.ActPAvgEff))
-		epcLog.SetCellFloat("Hid2 ActAvg", epc, float64(hid2Lay.Pools[0].ActAvg.ActPAvgEff))
-		epcLog.SetCellFloat("Out ActAvg", epc, float64(outLay.Pools[0].ActAvg.ActPAvgEff))
+		epcLog.SetFloat("Epoch", epc, float64(epc))
+		epcLog.SetFloat("CosDiff", epc, float64(outCosDiff))
+		epcLog.SetFloat("AvgCosDiff", epc, float64(outLay.CosDiff.Avg))
+		epcLog.SetFloat("SSE", epc, sse)
+		epcLog.SetFloat("Avg SSE", epc, avgSSE)
+		epcLog.SetFloat("Count Err", epc, float64(cntErr))
+		epcLog.SetFloat("Pct Err", epc, pctErr)
+		epcLog.SetFloat("Pct Cor", epc, pctCor)
+		epcLog.SetFloat("Hid1 ActAvg", epc, float64(hid1Lay.Pools[0].ActAvg.ActPAvgEff))
+		epcLog.SetFloat("Hid2 ActAvg", epc, float64(hid2Lay.Pools[0].ActAvg.ActPAvgEff))
+		epcLog.SetFloat("Out ActAvg", epc, float64(outLay.Pools[0].ActAvg.ActPAvgEff))
 	}
 	tmr.Stop()
 	if Silent {
-		fmt.Printf("%6.3g\n", tmr.TotalSecs())
+		fmt.Printf("%6.3g\n", tmr.Total)
 	} else {
-		fmt.Printf("Took %6.4g secs for %v epochs, avg per epc: %6.4g\n", tmr.TotalSecs(), epcs, tmr.TotalSecs()/float64(epcs))
-		net.TimerReport()
+		fmt.Printf("Took %v for %v epochs, avg per epc: m%6.4g\n", tmr.Total, epcs, float64(tmr.Total)/float64(int(time.Second)*epcs))
 	}
 }
 
 func main() {
-	var threads int
 	var epochs int
 	var pats int
 	var units int
@@ -230,7 +204,6 @@ func main() {
 	}
 
 	// process command args
-	flag.IntVar(&threads, "threads", 1, "number of threads (goroutines) to use")
 	flag.IntVar(&epochs, "epochs", 2, "number of epochs to run")
 	flag.IntVar(&pats, "pats", 10, "number of patterns per epoch")
 	flag.IntVar(&units, "units", 100, "number of units per layer -- uses NxN where N = sqrt(units)")
@@ -238,11 +211,11 @@ func main() {
 	flag.Parse()
 
 	if !Silent {
-		fmt.Printf("Running bench with: %v threads, %v epochs, %v pats, %v units\n", threads, epochs, pats, units)
+		fmt.Printf("Running bench with: %v epochs, %v pats, %v units\n", epochs, pats, units)
 	}
 
-	Net = &leabra.Network{}
-	ConfigNet(Net, threads, units)
+	Net = leabra.NewNetwork("Bench")
+	ConfigNet(Net, units)
 
 	Pats = &table.Table{}
 	ConfigPats(Pats, pats, units)
