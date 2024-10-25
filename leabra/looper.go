77842e75a6e7a2d07be8971f1bc9f9f68c2f1db5
Randall C. O'Reilly <oreilly@ucdavis.edu>
2024-10-25 12:49:57 -0700

    set testing flag on viewupdate

--- leabra/looper.go
+++ leabra/looper.go
@@ -126,6 +126,7 @@ func LooperUpdateNetView(man *looper.Manager, viewupdt *netview.ViewUpdate, net 
 			if curTime != etime.Cycle {
 				loop.OnEnd.Add("GUI:UpdateNetView", func() {
 					ctrUpdateFunc(curTime)
+					viewupdt.Testing = m == etime.Test
 					viewupdt.UpdateTime(curTime)
 				})
 			}
@@ -134,6 +135,7 @@ func LooperUpdateNetView(man *looper.Manager, viewupdt *netview.ViewUpdate, net 
 		cycLoop.OnEnd.Add("GUI:UpdateNetView", func() {
 			cyc := cycLoop.Counter.Cur
 			ctrUpdateFunc(etime.Cycle)
+			viewupdt.Testing = m == etime.Test
 			viewupdt.UpdateCycle(cyc)
 		})
 	}
